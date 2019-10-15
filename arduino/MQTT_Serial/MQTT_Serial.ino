3b3e5e2cad4e6a7e085ab4c3b1a2077a594b42a0
SangminPark <psm9733@naver.com>
2019-10-15 23:18:06 +0900

    Add: sns->get status, sns->change mqtt ip function

--- arduino/MQTT_Serial/MQTT_Serial.ino
+++ arduino/MQTT_Serial/MQTT_Serial.ino
@@ -1,8 +1,12 @@
+#include <EEPROM.h>
+
 const int modem_onoff_reset_pin = 2;
 const int gate_on_pin = 3;
 const int gate_off_pin = 4;
+int memory_length = 128;
 int pub_time_term = 1 * 60; //(mean of 1 is one min)
 unsigned long pre_time;
+unsigned long previousMillis = 0;
 boolean lock = true;
 boolean io_mode = true;
 boolean mqtt_ready = false;
@@ -13,6 +17,9 @@ String username = "\"hansung\"";
 String password = "\"hansung2019\"";
 String sub_topic = "\"command/gate/";
 String pub_topic = "\"status/gate/";
+String administrator = "01027079417";
+String modem_number;
+String sender;
 
 void Modem_reset(){
     mqtt_ready = false;
@@ -25,12 +32,69 @@ void Modem_reset(){
     digitalWrite(modem_onoff_reset_pin, HIGH);
 }
 
-void Model_at_cnum(){
+void Modem_init(){
     if(io_mode == true){
-        Serial.println("Called: Model_at_cnum()");
+        Serial.println("Called: Modem_init()");
+    }
+    sub_topic = "\"command/gate/";
+    pub_topic = "\"status/gate/";
+    Modem_sns_memory_reset();
+    String data;
+    byte count = 0;
+    if (is_memory() == true){
+        for(int index = 0; index < memory_length; index++){
+            char c = char(EEPROM.read(index));
+            data = String(data + c);
+            if (c == 34){         //34 = "\""
+                count++;
+            }
+            if (count == 2){
+                break;
+            }
+        }
+    }
+    mqttip = data;
+    if(io_mode == true){
+        Serial.println("Called: Modem_init()");
+        Serial.println("Send AT-Command: AT+CMGF=1");
+    }
+    Serial1.println("AT+CMGF=1");
+}
+
+void Modem_sns_memory_reset(){
+    if(io_mode == true){
+        Serial.println("Called: Modem_sns_memory_reset()");
+        Serial.println("Send AT-Command: AT+CMGD=,1");
+    }
+    Serial1.println("AT+CMGD=,1");
+}
+
+void Modem_cnum(){
+    if(io_mode == true){
+        Serial.println("Called: Modem_cnum()");
         Serial.println("Send AT-Command: AT+CNUM");
     }
     Serial1.println("AT+CNUM");
+}
+
+void Modem_cmgr(String message){
+    if(io_mode == true){
+        Serial.println("Called: Modem_cmgr()");
+        Serial.println("Send AT-Command: AT+CMGR");
+    }
+    int index = message.indexOf(",");
+    String message_index = message.substring(index + 1, message.length());
+    Serial1.println("AT+CMGR=" + String(message_index));
+}
+
+void Modem_cmgs(String message){
+    if(io_mode == true){
+        Serial.println("Called: Modem_cmgs()");
+        Serial.println("Send AT-Command: AT+CMGS");
+    }
+    Serial1.println("AT+CMGS=\"" + sender + "\"");
+    delay(100);
+    Serial1.println(message + "\x1a");
 }
 
 void Mqtt_init_topic(String message){
@@ -39,21 +103,21 @@ void Mqtt_init_topic(String message){
     }
     int index1 = message.indexOf("\"") + 1;
     int index2 = message.indexOf("\"", index1);
-    String modem_number = message.substring(index1, index2) + "\"";
-    sub_topic += modem_number;
-    pub_topic += modem_number;
+    modem_number = message.substring(index1, index2);
+    sub_topic += modem_number + "\"";
+    pub_topic += modem_number + "\"";
     mqtt_ready = true;
 }
 
-void MqttOpen(String ip, String port){
+void Mqtt_open(String ip, String port){
     if(io_mode == true){
         Serial.println("Called: MqttOpen()");
-        Serial.println("Send AT-Command: AT+QMTOPEN");
+        Serial.println("Send AT-Command: AT+QMTOPEN=0," + ip + "," + port);
     }
     Serial1.println("AT+QMTOPEN=0," + ip + "," + port);
 }
 
-void MqttConn(String id, String username, String pass){
+void Mqtt_conn(String id, String username, String pass){
     if(io_mode == true){
         Serial.println("Called: MqttConn()");
         Serial.println("Send AT-Command: AT+QMTCONN");
@@ -80,20 +144,61 @@ void Sub(String topic) {
     Serial1.println("AT+QMTSUB=0,1," + topic + ",2");
 }
 
+void Change_ip(String addr){
+    if (io_mode == true){
+        Serial.println("Called: Change_ip()");
+    }
+    addr = "\"" + addr + "\""; 
+    for(int index = 0; index < memory_length; index++){
+        EEPROM.write(index, 0);
+    }
+    for(int index = 0; index < addr.length(); index++){
+        EEPROM.write(index, addr[index]); 
+    }
+    Modem_reset();
+}
+
+boolean is_memory(){
+    if (io_mode == true){
+        Serial.println("Called: is_memory()");
+    }
+    if (EEPROM.read(0) == 34){
+        return true;
+    }
+    return false;
+}
+
 void MessageFilter(String message) {
     if(message.startsWith("ERROR")){
         Modem_reset();
     }else{
-      
-        if(message.startsWith("+CPIN:")){
-            Model_at_cnum();
+        if(message.startsWith("+QIND:")){
+            Modem_init();
+            Modem_cnum();
         }else if(message.startsWith("+CNUM:")){
             Mqtt_init_topic(message);
+            Mqtt_open(mqttip, mqttport);
+        }else if(message.startsWith("+CMTI:")){
+            delay(100);
+            Modem_cmgr(message);
+        }else if(message.startsWith("+CMGR:")){
+            int index1 = message.indexOf(",") + 2;
+            int index2 = message.indexOf(",", index1) - 1;
+            sender = message.substring(index1, index2);
+            Modem_sns_memory_reset();
+        }else if(message.startsWith("+changeip")){
+            int index = message.indexOf(":") + 1;
+            String addr = message.substring(index, message.length());
+            addr.trim();
+            Change_ip(addr);
+            //Modem_reset();
+        }else if(message.startsWith("+status")){
+            String info = "status: ok,\nmodem number: " + modem_number + ",\n" + "Mqtt IPaddr: " + mqttip;
+            Modem_cmgs(info);
         }
-
         if(mqtt_ready == true){
             if(message.startsWith("+QMTOPEN:")){
-                MqttConn(id, username, password);
+                Mqtt_conn(id, username, password);
             }else if(message.startsWith("+QMTCONN:")){
                 Sub(sub_topic);
             }else if(message.startsWith("+QMTSUB:")){
@@ -141,22 +246,21 @@ void loop() {
         if(io_mode == true){
             Serial.println("time = " + String(millis() / 60000) + " min");
         }
-        //Pub(pub_topic, "OK");
+        Pub(pub_topic, "ok");
         lock = true;
     }
 
-    //if(Serial.available()){
-    //    Serial1.write(Serial.read());
-    //}
+    if(Serial.available()){
+        Serial1.write(Serial.read());
+    }
+    
     if(Serial1.available()) {
         String message = Serial1.readStringUntil('\n');
         if(io_mode == true){
             Serial.print(message);
             Serial.print('\n'); 
         }
-        if(message.startsWith("+QIND:")){
-            MqttOpen(mqttip, mqttport);
-        }
         MessageFilter(message);
     }
+    
 }
